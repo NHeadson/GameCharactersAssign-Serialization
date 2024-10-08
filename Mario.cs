5b640e3d2325de0bffced2997f2a305b13de031f
Headsonn <nheadson@my.wctc.edu>
2024-10-08 13:57:19 -0500

    [1.08] Deserialize mario json from file into List<Mario> - Use debugger to examine marios - Set breakpoint on line that declares List<Mario>...

--- Mario.cs
+++ Mario.cs
@@ -1,6 +1,7 @@
 public class Mario : Character
 {
   public List<string> Alias { get; set; } = [];
+  
   public override string Display()
   {
     return $"Id: {Id}\nName: {Name}\nDescription: {Description}\nAlias: {string.Join(", ", Alias)}\n";
